4fa2f16e64cfb0c25ad22893e75540d371f92a35
sekta <akovlevmihail535@gmail.com>
2023-09-02 01:39:14 +0400

    UP COMING VOLUME

--- js/config.js
+++ js/config.js
@@ -122,7 +122,7 @@ var l_musicRandom = true;
 
 /*Music volume
 Choose a value between 0 and 100*/
-var l_musicVolume = 5;
+var l_musicVolume = 25;
 
 /*Enable custom messages?*/
 var l_messagesEnabled = true;
